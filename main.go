924afc31aa9191360e9d32325fe043bf0f8ff3cd
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-11-02 00:57:41 +0530

    create basic setup for server and whatsmeow client

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	// "mftpBridge/src/backend"
+	whatsapphandler "mftpBridge/src/whatsappHandler"
+)
+
+func main() {
+	// backend.StartBackend()
+	whatsapphandler.Connect()
+	
+}
